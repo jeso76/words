b003691be591af7813dea83bce6fb45eec8b7730
Jenny Söderberg <jenny@fnudd.com>
2018-10-28 22:04:59 +0100

    Initial commit for words

---
+++ src/main/java/se/home/dev/words/App.java
@@ -0,0 +1,13 @@
+package se.home.dev.words;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
